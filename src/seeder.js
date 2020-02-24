0486e7b064c3b1832467896905e21f7ddaf837f3
kostya.panin <kostya.panin@meliora.md>
2020-02-24 17:36:43 +0200

    Replaces custom text with lorem ipsum

--- src/seeder.js
+++ src/seeder.js
@@ -4,14 +4,14 @@ import { isObject } from './util';
 const ASSETS_DIR = '.';
 const data = new Map([
   [types.Title, 'Awesome title'],
-  [types.Text, 'We\'re creating the best place to go when starting a new business or company.With Baianat you can instantly search domain names, social media handles, and see your logo in beautiful logotypes.'],
+  [types.Text, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
   [types.Avatar, `${ASSETS_DIR}/img/avatar.png`],
   [types.Logo, `${ASSETS_DIR}/img/google.svg`],
   [types.Link, 'http://example.com'],
   [types.Image, `${ASSETS_DIR}/img/baianat.png`],
   [types.ClassList, () => []],
   [types.Button, () => ({ text: 'Click Me!', classes: [], href: 'http://example.com' })],
-  [types.Quote, 'When you were made a leader, you weren\'t given a crown; you were given the responsibility to bring out the best in others.'],
+  [types.Quote, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'],
   [types.Grid, () => ({mobile: '', tablet: '', desktop: '', widescreen: ''})],
   [Number, 100],
   [String, 'This is pretty neat']
